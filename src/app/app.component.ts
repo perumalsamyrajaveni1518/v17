9e0a52f646df7bee3b2205f079c987cc99f698a4
perumalsamys <perumalsamy.subburaj@veryxtech.com>
2024-01-29 18:31:47 +0530

    Added HTTP Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'v17';
+export class AppComponent implements OnInit {
+  title = "Test"
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get("/json/8.8.8.8").subscribe({
+      next:(res)=>{
+        console.log(res);
+      }
+    });
+    this.http.get("/json/8.8.8.8?fields=status,message,query,country,city").subscribe({
+      next:(res)=>{
+        console.log(res);
+      }
+    });
+  }
 }
